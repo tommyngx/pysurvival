93b26f9fbfc917735a3907b0961f82e80f804800
tommyngx <tommyfederation@gmail.com>
2024-12-12 20:16:41 +1100

    Update _coxph.pyx

--- pysurvival/cpp_extensions/_coxph.pyx
+++ pysurvival/cpp_extensions/_coxph.pyx
@@ -1,41 +1,37 @@
 # Import required modules
 cimport numpy as np
+import numpy as np
 from libcpp.vector cimport vector
-from numpy import ndarray
+from libcpp.memory cimport unique_ptr
 
 # Declare C++ class for Cox Proportional Hazards Model
 cdef extern from "functions.h":
     cdef cppclass _CoxPHModel:
-        void fit_model(vector[double] times, vector[double] events, vector[vector[double]] covariates)
-        vector[double] predict(vector[vector[double]] data)
+        _CoxPHModel() nogil
+        void fit_model(vector[double]& times, vector[double]& events, vector[vector[double]]& covariates) nogil
+        vector[double] predict(vector[vector[double]]& data) nogil
 
 # Define the Python wrapper class
 cdef class CoxPHModel:
-    cdef _CoxPHModel* cpp_model
+    cdef unique_ptr[_CoxPHModel] cpp_model
 
     def __cinit__(self):
         """
         Initialize the C++ model.
         """
-        self.cpp_model = new _CoxPHModel()
+        self.cpp_model = unique_ptr[_CoxPHModel](new _CoxPHModel())
 
-    def __dealloc__(self):
-        """
-        Clean up the C++ model.
-        """
-        del self.cpp_model
-
-    def fit(self, ndarray[np.float64_t, ndim=1] times, 
-            ndarray[np.float64_t, ndim=1] events, 
-            ndarray[np.float64_t, ndim=2] covariates):
+    def fit(self, np.ndarray[np.float64_t, ndim=1] times,
+            np.ndarray[np.float64_t, ndim=1] events,
+            np.ndarray[np.float64_t, ndim=2] covariates):
         """
         Fit the Cox proportional hazards model using the provided data.
         """
         cdef vector[double] c_times
         cdef vector[double] c_events
         cdef vector[vector[double]] c_covariates
-        cdef vector[double] row  # Declare vector outside the loop
-        cdef int i, j
+        cdef vector[double] row
+        cdef Py_ssize_t i, j
 
         # Convert NumPy arrays to C++ vectors
         for i in range(times.shape[0]):
@@ -43,35 +39,35 @@ cdef class CoxPHModel:
         for i in range(events.shape[0]):
             c_events.push_back(events[i])
         for i in range(covariates.shape[0]):
-            row.clear()  # Clear the vector to reuse it
+            row.clear()
             for j in range(covariates.shape[1]):
                 row.push_back(covariates[i, j])
             c_covariates.push_back(row)
 
-        self.cpp_model.fit_model(c_times, c_events, c_covariates)
+        self.cpp_model.get().fit_model(c_times, c_events, c_covariates)
 
-    def predict(self, ndarray[np.float64_t, ndim=2] data):
+    def predict(self, np.ndarray[np.float64_t, ndim=2] data):
         """
         Predict risk scores for the given data.
         """
         cdef vector[vector[double]] c_data
-        cdef vector[double] row  # Declare vector outside the loop
+        cdef vector[double] row
         cdef vector[double] c_predictions
-        cdef ndarray[np.float64_t, ndim=1] predictions
-        cdef int i, j
+        cdef np.ndarray[np.float64_t, ndim=1] predictions
+        cdef Py_ssize_t i, j
 
         # Convert NumPy array to C++ vector of vectors
         for i in range(data.shape[0]):
-            row.clear()  # Clear the vector to reuse it
+            row.clear()
             for j in range(data.shape[1]):
                 row.push_back(data[i, j])
             c_data.push_back(row)
 
-        c_predictions = self.cpp_model.predict(c_data)
+        c_predictions = self.cpp_model.get().predict(c_data)
 
         # Convert C++ vector to NumPy array
-        predictions = np.empty(len(c_predictions), dtype=np.float64)
-        for i in range(len(c_predictions)):
+        predictions = np.zeros(c_predictions.size(), dtype=np.float64)
+        for i in range(c_predictions.size()):
             predictions[i] = c_predictions[i]
 
         return predictions
